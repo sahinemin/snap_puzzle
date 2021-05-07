67056c1fd9f9fed4e30c6401b1dc756755033d05
albannablade123 <hasanal082000@gmail.com>
2021-05-07 22:53:43 +0300

    Updated theme design
    3rd commit, updated theme design

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -45,6 +45,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF003942),
         body: PageView(
           // physics: NeverScrollableScrollPhysics(),
           onPageChanged: (pageNo) {
@@ -87,16 +88,17 @@ class Page1 extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
+        color: Color(0xFF003942),
         height: 120.0,
         child: ListView.separated(
           separatorBuilder: (context, index) => Divider(
-            color: Colors.grey[560],
+            color: Colors.grey[400],
           ),
           itemCount: List1.length,
           itemBuilder: (BuildContext context, int index){
             return ListTile(
               leading: CircleAvatar(),
-              title: Text(List1[index]),
+              title: Text(List1[index], style: TextStyle(color: Colors.white)),
               subtitle: Text(
                 'Hey wanna see the image? heres a puzzle!',
                 overflow: TextOverflow.ellipsis,
