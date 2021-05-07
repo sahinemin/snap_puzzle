9f42a83920e5258f086b2be7231d4cf7306d206e
eminsahin <emin.sahin@agu.edu.tr>
2021-05-07 22:49:03 +0300

    profile

--- lib/profilescreen.dart
+++ lib/profilescreen.dart
@@ -14,105 +14,102 @@ class _profilescreenState extends State<profilescreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: SafeArea(
-            child: Scaffold(
-              floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-              body: SafeArea(
-                  child: Container(
-                    color: Color(0xFF003942),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                                padding: const EdgeInsets.all(8),
-                                decoration: new BoxDecoration(
-                                  shape: BoxShape.circle,
-                                ),
-                                height:200,
-                                child: CircleAvatar(
-                                  radius: 200,
-                                  backgroundImage: NetworkImage('https://placekitten.com/g/300/300'),
-                                )
-                            )
-                          ],
-                        ),
-                        Expanded(
-                          child: Stack(
+        home: Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+          body: Container(
+            color: Color(0xFF003942),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: new BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          height:200,
+                          child: CircleAvatar(
+                            radius: 200,
+                            backgroundImage: NetworkImage('https://placekitten.com/g/300/300'),
+                          )
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.all(8),
+                          child: Column(
                             children: [
                               Container(
                                   padding: const EdgeInsets.all(8),
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                          padding: const EdgeInsets.all(8),
-                                          //color: Colors.lightBlue,
-                                          color: Colors.orange,
-                                          width: 500,
-                                          child: Text(
-                                              "id: 1"
-                                          )
-                                      ),
-                                      Container(
-                                          padding: const EdgeInsets.all(8),
-                                          width: 500,
-                                          //color: Colors.lightBlue,
-                                          color: Colors.orange[300],
-                                          child: Text(
-                                              "username: Hikmet42"
-                                          )
-                                      ),
-                                      Container(
-                                          padding: const EdgeInsets.all(8),
-                                          width: 500,
-                                          //color: Colors.lightBlue,
-                                          color: Colors.orange,
-                                          child: Text(
-                                              "name: Hikmet Can"
-                                          )
-                                      ),
-                                      Container(
-                                          padding: const EdgeInsets.all(8),
-                                          width: 500,
-                                          //color: Colors.lightBlue,
-                                          color: Colors.orange[300],
-                                          child: Text(
-                                              "surname: Doğancı"
-                                          )
-                                      ),
-                                      Container(
-                                          padding: const EdgeInsets.all(8),
-                                          width: 500,
-                                          color: Colors.orange,
-                                          //color: Colors.lightBlue,
-                                          child: Text(
-                                              "School name: Agü"
-                                          )
-                                      ),
-                                    ],
+                                  //color: Colors.lightBlue,
+                                  color: Colors.orange,
+                                  width: 500,
+                                  child: Text(
+                                      "id: 1"
                                   )
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      FloatingActionButton(child: Icon(Icons.settings)),
-                                    ],
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  width: 500,
+                                  //color: Colors.lightBlue,
+                                  color: Colors.orange[300],
+                                  child: Text(
+                                      "username: Hikmet42"
                                   )
-                                ],
-                              )
+                              ),
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  width: 500,
+                                  //color: Colors.lightBlue,
+                                  color: Colors.orange,
+                                  child: Text(
+                                      "name: Hikmet Can"
+                                  )
+                              ),
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  width: 500,
+                                  //color: Colors.lightBlue,
+                                  color: Colors.orange[300],
+                                  child: Text(
+                                      "surname: Doğancı"
+                                  )
+                              ),
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  width: 500,
+                                  color: Colors.orange,
+                                  //color: Colors.lightBlue,
+                                  child: Text(
+                                      "School name: Agü"
+                                  )
+                              ),
                             ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-
-              ),
-            )
+                          )
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              FloatingActionButton(child: Icon(Icons.settings)),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         )
     );
   }
