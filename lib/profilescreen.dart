862a0e63265c39c622457bedecb395317ff03bb7
albannablade123 <hasanal082000@gmail.com>
2021-05-29 18:04:59 +0300

    sixth commit - Hasan
    Changes in layout and color design

--- lib/profilescreen.dart
+++ lib/profilescreen.dart
@@ -30,122 +30,123 @@ class _profilescreenState extends State<profilescreen> {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: SafeArea(
-            child: Scaffold(
-              backgroundColor: Colors.white,
-              floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-              floatingActionButton: FloatingActionButton(
-                  backgroundColor: Colors.green.shade700,
-                  child: Icon(Icons.settings),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/Settings');
-                  }),
-              body: SafeArea(
-                child: Column(children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+            floatingActionButton: FloatingActionButton(
+                backgroundColor: Colors.green.shade700,
+                child: Icon(Icons.settings),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/Settings');
+                }),
+            body: SafeArea(
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Container(
+                      width: double.infinity,
+                      height: 110,
+                      child: Container(
+                        alignment: Alignment(0.0, 2.5),
+                        child: CircleAvatar(
+                          radius: 55.0,
+                          backgroundImage: NetworkImage(profilescreen.photo),
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Username123',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '${profilescreen.fullname}"',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.grey[500]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Container(
+                    width: 150.0,
+                    color: Colors.transparent,
                     child: Container(
-                        width: double.infinity,
-                        height: 110,
-                        child: Container(
-                          alignment: Alignment(0.0, 2.5),
-                          child: CircleAvatar(
-                            radius: 55.0,
-                            backgroundImage: NetworkImage(profilescreen.photo),
+                        decoration: BoxDecoration(
+                          color: Colors.white70,
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 1,
+                              blurRadius: 10,
+                              offset:
+                                  Offset(0, 4), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: new Center(
+                          child: new Text(
+                            "1000 pts",
+                            style: TextStyle(
+                                color: Colors.redAccent[400],
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
                           ),
                         )),
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    'Username123',
-                    style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    '${profilescreen.fullname}"',
-                    style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w200,
-                        color: Colors.grey[500]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(6.0),
-                    child: Container(
-                      width: 150.0,
-                      color: Colors.transparent,
-                      child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white70,
-                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.3),
-                                spreadRadius: 1,
-                                blurRadius: 10,
-                                offset: Offset(0, 4), // changes position of shadow
-                              ),
-                            ],
-                          ),
-                          child: new Center(
-                            child: new Text(
-                              "1000 pts",
-                              style: TextStyle(
-                                  color: Colors.redAccent[400],
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.center,
-                            ),
-                          )),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  CarouselSlider(
-                    options: CarouselOptions(
-                      height: MediaQuery.of(context).size.height * 0.40,
-                      enableInfiniteScroll: false,
-                    ),
-                    items: ['Badges', 'Statistics', 'Info'].map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          switch (i) {
-                            case 'Badges':
-                              {
-                                return BadgeContainer();
-                              }
-                            case 'Statistics':
-                              {
-                                return StatisticsContainer();
-                              }
-                            default:
-                              {
-                                return UserInfoContainer();
-                              }
-                          }
-                        },
-                      );
-                    }).toList(),
-                  ),
-                  FlatButton(
-                    child: Text(
-                      'LOG OUT',
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black,
-                      ),
-                    ),
-                    onPressed: () async {
-                      await googlelogout();
-                      Navigator.of(context).pushNamed('/LogIn');
-                    },
-                  ),
-                ]
                 ),
-              ),
-            ),));
+                SizedBox(
+                  height: 15,
+                ),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height * 0.34,
+                    enableInfiniteScroll: false,
+                  ),
+                  items: ['Badges', 'Statistics', 'Info'].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        switch (i) {
+                          case 'Badges':
+                            {
+                              return BadgeContainer();
+                            }
+                          case 'Statistics':
+                            {
+                              return StatisticsContainer();
+                            }
+                          default:
+                            {
+                              return UserInfoContainer();
+                            }
+                        }
+                      },
+                    );
+                  }).toList(),
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  child: Text(
+                    'LOG OUT',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () async {
+                    await googlelogout();
+                    Navigator.of(context).pushNamed('/LogIn');
+                  },
+                ),
+              ]),
+            ),
+          ),
+        ));
   }
-
 
   Future googlelogout() async {
     try {
@@ -153,7 +154,6 @@ class _profilescreenState extends State<profilescreen> {
       //await _googleSignIn.disconnect();
       await _googleSignIn.signOut();
       print(_googleSignIn.currentUser.id);
-
     } catch (e) {
       print(e.toString());
     }
@@ -324,7 +324,7 @@ class StatisticsContainer extends StatelessWidget {
                                   color: Colors.green[900],
                                   fontWeight: FontWeight.bold),
                               fontSize:
-                              MediaQuery.of(context).textScaleFactor * 12,
+                                  MediaQuery.of(context).textScaleFactor * 12,
                             ),
                           ),
                         ),
@@ -334,7 +334,7 @@ class StatisticsContainer extends StatelessWidget {
                             textStyle: TextStyle(
                               color: Colors.green[900],
                               fontSize:
-                              MediaQuery.of(context).textScaleFactor * 13,
+                                  MediaQuery.of(context).textScaleFactor * 13,
                             ),
                           ),
                         ),
@@ -353,7 +353,8 @@ class StatisticsContainer extends StatelessWidget {
                           'Amount Solved',
                           style: GoogleFonts.roboto(
                             textStyle: TextStyle(
-                                fontSize: MediaQuery.textScaleFactorOf(context) * 17,
+                                fontSize:
+                                    MediaQuery.textScaleFactorOf(context) * 17,
                                 color: Colors.green[900],
                                 fontWeight: FontWeight.bold),
                           ),
@@ -424,8 +425,8 @@ class StatisticsContainer extends StatelessWidget {
                                   color: Colors.green[900],
                                   fontWeight: FontWeight.bold,
                                   fontSize:
-                                  MediaQuery.of(context).textScaleFactor *
-                                      12),
+                                      MediaQuery.of(context).textScaleFactor *
+                                          12),
                             ),
                           ),
                         ),
@@ -475,27 +476,39 @@ class BadgeContainer extends StatelessWidget {
               ]),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 7.0),
+              padding: EdgeInsets.symmetric(vertical: 4.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.17,
+                    width: MediaQuery.of(context).size.width * 0.18,
                     height: MediaQuery.of(context).size.height * 0.12,
-                    child: Center(child: Text('Badge 1')),
-                    color: Colors.orange,
+                    decoration: new BoxDecoration(
+                      color: Colors.deepPurple,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.emoji_events_outlined,
+                        color: Colors.yellow, size: 60),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.17,
-                    height: MediaQuery.of(context).size.height * 0.12,
-                    child: Center(child: Text('Badge 1')),
-                    color: Colors.orangeAccent,
+                    width: MediaQuery.of(context).size.width * 0.18,
+                    height: MediaQuery.of(context).size.height * 0.14,
+                    decoration: new BoxDecoration(
+                      color: Colors.yellow,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.emoji_events_outlined,
+                        color: Colors.grey, size: 60),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.17,
+                    width: MediaQuery.of(context).size.width * 0.18,
                     height: MediaQuery.of(context).size.height * 0.12,
-                    child: Center(child: Text('Badge 1')),
-                    color: Colors.deepOrange,
+                    decoration: new BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.emoji_events_outlined,
+                        color: Colors.brown, size: 60),
                   ),
                 ],
               ),
@@ -506,22 +519,34 @@ class BadgeContainer extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.17,
+                    width: MediaQuery.of(context).size.width * 0.18,
                     height: MediaQuery.of(context).size.height * 0.12,
-                    child: Center(child: Text('Badge 1')),
-                    color: Colors.blueAccent,
+                    decoration: new BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.star,
+                        color: Colors.yellow, size: 60),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.17,
+                    width: MediaQuery.of(context).size.width * 0.18,
                     height: MediaQuery.of(context).size.height * 0.12,
-                    child: Center(child: Text('Badge 1')),
-                    color: Colors.blue[400],
+                    decoration: new BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.military_tech_outlined,
+                        color: Colors.yellow, size: 60),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.17,
+                    width: MediaQuery.of(context).size.width * 0.18,
                     height: MediaQuery.of(context).size.height * 0.12,
-                    child: Center(child: Text('Badge 1')),
-                    color: Colors.blue[200],
+                    decoration: new BoxDecoration(
+                      color: Colors.green[200],
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.celebration_outlined,
+                        color: Colors.yellow, size: 60),
                   ),
                 ],
               ),
