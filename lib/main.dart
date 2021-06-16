fb24143318be1efed31f47b8be8fa94909aad368
eminsahin <emin.sahin@agu.edu.tr>
2021-06-16 22:18:24 +0300

    son
    Co-Authored-By: meKillua <56931792+meKillua@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 import 'RouteGenerator.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-AndroidNotificationChannel channel = const AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.high,
-);
 
 
 Future  main() async {
